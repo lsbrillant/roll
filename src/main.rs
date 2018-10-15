097f2df27e5b3c24d69ab7832acb278a2ee6298b
Lucien Brillant <lucienbrillant@gmail.com>
2018-10-14 18:29:06 -0700

    add usage

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@ extern crate roll;
 use roll::ast::Eval;
 
 fn main() -> Result<(), Box<std::error::Error>> {
-    let line: String = args().skip(1).collect::<Vec<String>>().join(" ");
+    let args = args().skip(1).collect::<Vec<String>>();
+    if args.iter().count() < 1 {
+        println!("Usage roll <dice>");
+        return Ok(())
+    }
+    let line: String = args.join(" ");
     let result = roll::parse::parse_expr(&mut roll::scan::Scanner::new(line.chars()))?.eval();
     println!("{}", result);
     Ok(())
